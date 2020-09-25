3ba15f12629c161a9ecb832614b3d91506c7674a
Kenobi17 <matiascorrea420@gmail.com>
2020-09-25 12:55:36 -0300

    Edit db.js, fixed enviroment variables

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const db = new Pool({
-  user: PGUSER,
-  password: PGPASSWORD,
-  database: PGDATABASE,
-  host: PGHOST,
-  port: PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
 });
 
 module.exports = db;
