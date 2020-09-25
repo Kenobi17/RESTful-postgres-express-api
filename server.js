a1c207b2c5f7e94d671bd25ce3b8e3179ccce0d8
Kenobi17 <matiascorrea420@gmail.com>
2020-09-25 13:31:54 -0300

    Add all the RESTful routes with their respective CRUD operations

--- server.js
+++ server.js
@@ -4,6 +4,92 @@ const express = require("express"),
   db = require("./db"),
   app = express();
 
+app.use(express.json());
+
+//ROUTES//
+
+//GET ALL TODOS
+app.get("/api/v1", async (req, res) => {
+  try {
+    const allTodos = await db.query("SELECT * FROM todo");
+    res.json({
+      status: "success",
+      data: {
+        todos: allTodos.rows,
+      },
+    });
+  } catch {
+    console.error(err.message);
+  }
+});
+
+//GET A TODO
+app.get("/api/v1/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await db.query("SELECT * FROM todo WHERE id = $1", [id]);
+    res.json({
+      status: "success",
+      data: {
+        todo: todo.rows[0],
+      },
+    });
+  } catch {
+    console.error(err.message);
+  }
+});
+
+//CREATE A TODO
+app.post("/api/v1", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await db.query(
+      "INSERT INTO todo (description) VALUES ($1) RETURNING *",
+      [description]
+    );
+    res.json({
+      status: "success",
+      data: {
+        todo: newTodo.rows[0],
+      },
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+//UPDATE A TODO
+app.put("/api/v1/:id", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const { id } = req.params;
+    const updatedTodo = await db.query(
+      "UPDATE todo SET description = $1 WHERE id = $2 RETURNING *",
+      [description, id]
+    );
+    res.json({
+      status: "success",
+      data: {
+        todo: updatedTodo.rows[0],
+      },
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//DELETE A TODO
+app.delete("/api/v1/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedTodo = await db.query("DELETE FROM todo WHERE id = $1", [id]);
+    res.json({
+      status: "Todo deleted!",
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`API SERVER RUNNING AT http://localhost:${PORT}`);
 });
